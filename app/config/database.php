28d7047050544b55eb37bb2062a6936d87ec125b
nimbomx <ernesto@nimbo.mx>
2014-11-27 13:38:43 -0600

    fail

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'forge',
 			'username'  => 'forge',
-			'password'  => 'UltraSecret',
+			'password'  => 'UltraSecret2',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
