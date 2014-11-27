fe23d0701215e71fe991903b295569d2082d9d61
nimbomx <ernesto@nimbo.mx>
2014-11-27 13:50:58 -0600

    WTF!!!!

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -25,7 +25,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'homestead',
 			'username'  => 'homestead',
-			'password'  => 'secretOneTwo',
+			'password'  => 'secret',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
