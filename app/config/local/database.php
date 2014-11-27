c1f14e6e9291d8ff33de446cb4fb8d964efa1d84
nimbomx <ernesto@nimbo.mx>
2014-11-27 13:42:37 -0600

    update git

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -25,7 +25,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'homestead',
 			'username'  => 'homestead',
-			'password'  => 'secretOne',
+			'password'  => 'secretOneTwo',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
