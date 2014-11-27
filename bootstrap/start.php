888c3030a0d28c48eebd2922401a1965b94f6ed8
nimbomx <ernesto@nimbo.mx>
2014-11-27 14:32:51 -0600

    Install Imagine
    Composer Update

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('ehernandez-pc'),
+	'local' => array('Nimbo-desktop','ehernandez-pc'),
 
 ));
 
